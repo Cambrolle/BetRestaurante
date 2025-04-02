fc3902b6b4a41fc7d09ca10375a81c6cee2ce74d
Alejandro <acambrollemoraza@safareyes.es>
2025-04-02 16:45:44 +0200

    Proyecto3

--- BetRestaurante/urls.py
+++ BetRestaurante/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls.conf import include
+
+from polls import views
 
 urlpatterns = [
+    path('', include('polls.urls')),
     path('admin/', admin.site.urls),
+    path('polls/' , include('polls.urls')),
 ]
--- polls/urls.py
+++ polls/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
 from . import views
+from .views import *
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path('', home_page, name="home"),
+    path('home/', home_page, name="home"),
 ]
