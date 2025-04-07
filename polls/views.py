0fe457a29872b453829fceb84590664d0fa9695b
Alejandro <acambrollemoraza@safareyes.es>
2025-04-07 15:47:04 +0200

    we

--- polls/views.py
+++ polls/views.py
@@ -7,6 +7,6 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 def home_page(request):
-    return render(request, 'home.html')
+    return render(request, 'inicio.html')
 def base_page(request):
     return render(request, 'base.html')
