3ae995220044136892d65d2274fb4bc765d53516
Alejandro <acambrollemoraza@safareyes.es>
2025-05-26 15:34:35 +0200

    Proyecto5

--- BetRestaurante/urls.py
+++ BetRestaurante/urls.py
@@ -14,18 +14,30 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import path
-from polls.views import *
 
+from polls.views import *
+from django.contrib import admin
+from django.urls import path, include
+from polls import views as polls_views
 urlpatterns = [
-    path('', home_page, name="home"),
-    path('inicio/', home_page, name="home"),
-    path('base/', base_page, name="base"),
-    path('crear_cuenta/', registrar_usuario, name="crear"),
-    path('iniciar_sesion/', login_usuario, name="iniciar"),
-    path('cerrar_sesion/', logout_usuario, name="logout"), -
-    path('pagina_gestion/', gestion_page, name="gestionar"),
-    path('mesas/', mesas_page, name="mesas"),
-    path('mesas/cambiar_estado/<int:mesa_id>/', cambiar_estado_mesa, name="cambiar_estado_mesa"),
+    path('', include('polls.urls')),
+    path('bet/', include('polls.urls')),
+    # path('inicio/', home_page, name="home"),
+    # path('base/', base_page, name="base"),
+    # path('crear_cuenta/', registrar_usuario, name="crear"),
+    # path('iniciar_sesion/', login_usuario, name="iniciar"),
+    # path('cerrar_sesion/', logout_usuario, name="logout"),
+    # path('pagina_gestion/', gestion_page, name="gestionar"),
+    # path('mesas/', mesas_page, name="mesas"),
+    # path('mesas/cambiar_estado/<int:mesa_id>/', cambiar_estado_mesa, name="cambiar_estado_mesa"),
 ]
 
+urlpatterns = [
+
+    path('', include('polls.urls')),  # Importante: incluye aquí tu app
+    path('prohibido/', polls_views.prueba_403),  # para probar 403
+]
+
+# Handlers de errores personalizados
+handler403 = 'BetRestaurante.views.handler403'
+handler404 = 'BetRestaurante.views.handler404'
